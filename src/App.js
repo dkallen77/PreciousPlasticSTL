a507736d0acdf1ad7759349da1bca71e33cc6569
Casey MacDonald <c.macdonald@wustl.edu>
2020-04-29 18:23:29 -0500

    Created MainPicture Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
+import MainPicture from './MainPicture';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <MainPicture />
     </div>
   );
 }
---
+++ src/MainPicture.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPicture.css';
+
+function MainPicture() {
+  return (
+    <div className="MainPicture">
+     HELLO
+    </div>
+
+  );
+}
+
+export default MainPicture;
