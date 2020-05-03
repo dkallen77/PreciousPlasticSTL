0126ca6f81efb7461b37c182d632b4aac503ec06
Casey MacDonald <c.macdonald@wustl.edu>
2020-05-03 12:50:54 -0500

    video embeded and working properly

--- src/OurSolution.js
+++ src/OurSolution.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './OurSolution.css';
+import clean from './img/clean.mp4';
 
 class OurSolution extends Component {
     constructor(props) {
       super(props);
     }
+
+    playVid(){
+        var vid = document.getElementById("cleanVid");
+        vid.play();
+    }
   
     render(){
         return (
-            <iframe src="https://giphy.com/embed/YOvuKHZwPrp8tr1deu" width="480" height="342" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/YOvuKHZwPrp8tr1deu">via GIPHY</a></p>
+            <div className="OurSolution">
+                <video id="cleanVid" width="320" height="176" onClick={this.playVid}>
+                    <source src={clean} type="video/mp4"/>
+                    Your browser does not support HTML5 video.
+                </video>
+            </div>
         );
     }
 }
