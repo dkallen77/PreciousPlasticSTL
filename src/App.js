790312329e024fc7afbd35832d06cb250aeac572
Casey MacDonald <c.macdonald@wustl.edu>
2020-05-03 12:28:37 -0500

    OurSolution section created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import MainPicture from './MainPicture';
+import OurSolution from './OurSolution';
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <MainPicture />
+        <OurSolution />
       </div>
     );
 }
---
+++ src/OurSolution.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './OurSolution.css';
+
+class OurSolution extends Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render(){
+        return (
+            <iframe src="https://giphy.com/embed/YOvuKHZwPrp8tr1deu" width="480" height="342" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/YOvuKHZwPrp8tr1deu">via GIPHY</a></p>
+        );
+    }
+}
+
+export default OurSolution;
