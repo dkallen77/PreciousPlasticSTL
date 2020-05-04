e57316d9e4e72f5a082bfa9e19c73a8e3b439e81
David Allen <dkallen77@gmail.com>
2020-05-03 22:00:35 -0500

    Updates

---
+++ precache-manifest.eb583117670bd948225a928b0df42267.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41e14965afc244a9c839d2f9db6fb0",
+    "url": "/PreciousPlasticSTL/index.html"
+  },
+  {
+    "revision": "2833b43caa6cf3348d68",
+    "url": "/PreciousPlasticSTL/static/css/main.7e59ccf4.chunk.css"
+  },
+  {
+    "revision": "3ddb80e4e1a26d0af7a2",
+    "url": "/PreciousPlasticSTL/static/js/2.8692fd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PreciousPlasticSTL/static/js/2.8692fd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2833b43caa6cf3348d68",
+    "url": "/PreciousPlasticSTL/static/js/main.a6956b60.chunk.js"
+  },
+  {
+    "revision": "7c2542da4df90d981d86",
+    "url": "/PreciousPlasticSTL/static/js/runtime-main.18542632.js"
+  },
+  {
+    "revision": "fa1e2c609414d422ea32b019fb8adcaa",
+    "url": "/PreciousPlasticSTL/static/media/Precious-Plastic-Logo.fa1e2c60.png"
+  },
+  {
+    "revision": "76c880b888e578a65bbd57bef7dc9755",
+    "url": "/PreciousPlasticSTL/static/media/clean.76c880b8.mp4"
+  },
+  {
+    "revision": "955e22d47e8920e0e4c63350997f62b7",
+    "url": "/PreciousPlasticSTL/static/media/collect2.955e22d4.png"
+  },
+  {
+    "revision": "968d027be87b187962a72a9fab3a1dda",
+    "url": "/PreciousPlasticSTL/static/media/community2.968d027b.png"
+  },
+  {
+    "revision": "e112b8880747ce75e04c7788288dc9c5",
+    "url": "/PreciousPlasticSTL/static/media/dirty_bottle.e112b888.png"
+  },
+  {
+    "revision": "b5e72db5312a9ff6f46d9fcc2b0c6672",
+    "url": "/PreciousPlasticSTL/static/media/join2.b5e72db5.png"
+  },
+  {
+    "revision": "08ccdd9c02acc5d36f1889abf1425a63",
+    "url": "/PreciousPlasticSTL/static/media/recycle2.08ccdd9c.png"
+  },
+  {
+    "revision": "ad2636074787385320274006311d8e58",
+    "url": "/PreciousPlasticSTL/static/media/shred.ad263607.mp4"
+  },
+  {
+    "revision": "405c62c5fc0717bf49baf9a8cc1f4e7d",
+    "url": "/PreciousPlasticSTL/static/media/transform2.405c62c5.png"
+  }
+]);
