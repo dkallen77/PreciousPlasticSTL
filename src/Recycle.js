3647710d2c8762c858c52ba9290e4c20a8c0fef1
David Allen <dkallen77@gmail.com>
2020-05-04 05:56:37 -0500

    more javascript for animations

--- src/Recycle.js
+++ src/Recycle.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './recycleComponents/Recycle.css'
 import background from "./recycleComponents/recycleBackground.png"
+import RecycleForm from "./recycleComponents/RecycleForm"
+import { wait } from '@testing-library/react';
 
 class Recycle extends Component {
 
@@ -16,18 +18,72 @@ class Recycle extends Component {
                         enough or don't work and then also a button to
                         expand the page*/}
                 </div>
-                <div id="recycleTransition">
-                    <img id="background" src={background} alt="ahh" />
-                </div>
                 <div id="recycleExpansion">
                     {/* This would be more complicated. Have embeded map
                         to show location and a button to trigger API to get
                         weather information. Put up the current information  */}
+                        <button id="expander" onClick={this.expand}>
+                            <svg  viewBox="-0.5 -0.5 11 11">
+                                <path d="M 10 0 L 5 10 L 0 0"></path>
+                            </svg>  
+                        </button>
+                        <h1>Is today a good day to Recycle?</h1>
+                        <RecycleForm />
+                </div>
+                <div id="recycleTransition">
+                    <img id="background" src={background} alt="ahh" />
                 </div>
             </div>
         );
     }
 
+    state = {
+        expanded: false
+    }
+
+    expand = (event) => {
+        var button = document.getElementById("expander");
+        document.getElementById("expander").disabled = true;
+        var expansion = document.getElementById("recycleExpansion");
+        var  expand = (this.state.expanded === false) ? true : false;
+        this.setState({
+            expanded: !(this.state.expanded)
+        });
+        //let's try for a good animation
+        
+        if (expand) {
+            button.style.transform = "rotate(180deg)";
+            var height = 0;
+            var id = setInterval(frame, 0);
+            function frame() {
+              if (height === 500) {
+                clearInterval(id);
+                button.disabled = false;
+              } 
+              else {
+                height++;
+                expansion.style.height = height + 'px';
+                button.style.bottom = "10px";
+              }
+            }
+        }
+        else {
+            button.style.transform = "rotate(0deg)";
+            var height = 500;
+            var id = setInterval(frame, 0);
+            function frame() {
+              if (height === 0) {
+                clearInterval(id);
+                button.disabled = false;
+              } 
+              else {
+                height--;
+                expansion.style.height = height + 'px';
+                button.style.bottom = "10px";
+              }
+            }
+        }
+    }
 }
 
 export default Recycle;
---
+++ src/recycleComponents/RecycleForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './RecycleForm.css'
+
+class RecycleForm extends Component {
+
+
+    render() {
+        return(
+            //html goes here
+            <form id="expansionButon">
+
+            </form>
+        );
+    }
+
+}
+
+export default RecycleForm;
